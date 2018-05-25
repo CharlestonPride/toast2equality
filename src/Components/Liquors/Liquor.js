4b6297ab3b60904cdfe31b48d11dad2c0f86cea2
Pete Bingel <peter.bingel@charlestonpride.org>
2018-05-24 20:30:47 -0400

    Implemented liquor section

--- src/Components/Liquors/Liquor.js
+++ src/Components/Liquors/Liquor.js
@@ -3,13 +3,14 @@ import React, { Component } from 'react'
 export default class Liquor extends Component {
   render(props) {
     return (
-        <div className="col-12 col-md-4 col-lg-2 ">
+        <div className="col-6 col-md-4 col-lg-2">
             <div className="">
                 <div className="liquor">
                     <img src={this.props.image} className="img-fluid" />
                     <div className="gb-overlay">
                         <div className="overlay-content">
-                            <a  className="image-link"><i className="icofont icofont-open-eye"></i></a>
+                            <h4 className="font-pacifico">{this.props.name}</h4>
+                            <a href={this.props.url} target="_blank"><i className="icofont icofont-web"></i></a>
                         </div>
                     </div>
                 </div>
--- src/Components/Liquors/Liquors.js
+++ src/Components/Liquors/Liquors.js
@@ -10,17 +10,21 @@ import './Liquors.css';
 
 export default class Liquors extends Component {
   render() {
-    return (
+      const stoli = "https://stoli.com/?utm_source=charleston_pride&utm_medium=toast_to_equality&utm_campaign=sponsors"
+      const jack = "https://www.jackdaniels.com/en-us/whiskey/tennessee-honey/?utm_source=charleston_pride&utm_medium=toast_to_equality&utm_campaign=sponsors"
+      const camerena = "http://www.tequilacamarena.com/tequilas/reposado?utm_source=charleston_pride&utm_medium=toast_to_equality&utm_campaign=sponsors"
+      const rumHaven = "http://www.rumhaven.com/?utm_source=charleston_pride&utm_medium=toast_to_equality&utm_campaign=sponsors"
+      return (
         <div >
             <div className="container-fluid text-center">
                 <h1 className="font-pacifico">Meet the Liquors</h1>
                 <div className="row">
-                    <Liquor image={Grapefruit} />
-                    <Liquor image={Camarena} />
-                    <Liquor image={Pineapple} />
-                    <Liquor image={Honey} />
-                    <Liquor image={Mango} />
-                    <Liquor image={RumHaven} />
+                      <Liquor image={Grapefruit} name="Stoli Crushed Ruby Red Grapefruit" url={stoli + "&utm_content=grapefruit"}/>
+                    <Liquor image={Camarena} name="Camerena Tequila Reposado" url={camerena}/>
+                      <Liquor image={Pineapple} name="Stoli Crushed Pineapple" url={stoli + "&utm_content=pineapple"}/>
+                    <Liquor image={Honey} name="Jack Daniels Tennessee Honey" url={jack}/>
+                      <Liquor image={Mango} name="Stoli Crushed Mango" url={stoli + + "&utm_content=mango"}/>
+                    <Liquor image={RumHaven} name="RumHaven Coconut Rum" url={rumHaven}/>
                 </div>
             </div>
         </div>

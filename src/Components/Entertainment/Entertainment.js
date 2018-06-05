cd754edf822359944f1d8be1b43f098296746c25
Pete Bingel <peter.bingel@charlestonpride.org>
2018-06-04 23:27:00 -0400

    Added Google Analytics Updated Entertainment

--- src/Components/Entertainment/Entertainment.js
+++ src/Components/Entertainment/Entertainment.js
@@ -4,23 +4,31 @@ import './Entertainment.css';
 export default class Entertainment extends Component {
   render() {
     return (
-        <div id="entertainment" className="gb-convenience section-before section-padding gb-parallax">
+        <div id="entertainment" className="gb-convenience section-padding gb-parallax text-center">
+            <div className="section-title">
+                <h1>Entertainment</h1>
+            </div>
             <div className="container text-center">
                 <div className="row">
-                    <div className="col-sm-6">
-                        <div className="convenience">
-                            <span className="icon"><i className="icofont icofont-microphone"></i></span>
-                            <h1>MC</h1>
-                            <h2>Mike Edwards </h2>
-                            <h3>Mix 95.9 Two Girls &amp; a Guy</h3>
-                        </div>
-                    </div>
-                    <div className="col-sm-6">
+                    <div className="col-sm-4">
                         <div className="convenience">
                             <span className="icon"><i className="icofont icofont-retro-music-disk"></i></span>
-                            <h1>Music</h1>
-                            <h2>DJ Trevor D</h2>
-                            <h3>D&amp;M Productions</h3>
+                            <h1>DJ Trevor D</h1>
+                            <h2></h2>
+                        </div>
+                    </div>
+                    <div className="col-sm-4">
+                        <div className="convenience">
+                            <span className="icon"><i className="icofont icofont-microphone"></i></span>
+                            <h1>MC Mike Edwards </h1>
+                            <h2>Mix 95.9 Two Girls &amp; a Guy</h2>
+                        </div>
+                    </div>
+                    <div className="col-sm-4">
+                        <div className="convenience">
+                            <span className="icon"><i className="icofont icofont-guiter"></i></span>
+                            <h1>Live Bands</h1>
+                            <h2></h2>
                         </div>
                     </div>
                 </div>

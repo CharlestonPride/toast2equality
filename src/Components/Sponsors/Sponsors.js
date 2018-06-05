2e50d968f386ea99717cc7d40aa7cc5d85c62a8e
Pete Bingel <peter.bingel@charlestonpride.org>
2018-06-05 18:16:00 -0400

    Added Dudleys as sponsor

--- src/Components/Sponsors/Sponsors.js
+++ src/Components/Sponsors/Sponsors.js
@@ -7,7 +7,7 @@ import Blackbaud from './blackbaud.jpg'
 import DavidAylor from './davidaylor.jpg'
 import InvitationOnly from './invitationOnly.png'
 import DMProductions from './dmproductions.png'
-
+import Dudleys from './dudleys.jpg'
 
 export default class Sponsors extends Component {
     
@@ -53,6 +53,7 @@ export default class Sponsors extends Component {
                 <Sponsor name="Mix 95.9" image={Mix959} facebook="mix959" twitter="mix959live" instagram="mix959live" web="http://mix959.com" level="Promoting Sponsor"/>
                 <Sponsor name="Invitation Only" image={InvitationOnly} facebook="InvitationOnlyLLC" twitter="" instagram="" web="https://www.invitationonlycharleston.com" level="Supporting Sponsor"/>
                 <Sponsor name="Blackbaud" image={Blackbaud} facebook="blackbaud" twitter="blackbaud" instagram="blackbaud" web="https://www.blackbaud.com" level="Presenting Sponsor"/>
+                <Sponsor name="Dudley's on Ann" image={Dudleys} facebook="dudleysonann" twitter="dudleysonann" instagram="dudleysonann" web="http://www.dudleysonann.com" level="Presenting Sponsor" />
                 <Sponsor name="David Aylor Law Firm" image={DavidAylor} facebook="AylorLaw" twitter="DavidAylor" instagram="davidaylorlawoffices" web="https://davidaylor.com/" level="Promoting Sponsor"/>
                 <Sponsor name="D&amp;M Productions" image={DMProductions} facebook="chsweddingdj" twitter="" instagram="dandm_productions" web="https://www.alowcountryevent.com/" level="Supporting Sponsor"/>
             </Slider>

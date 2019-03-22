8c0632bc9614e6f4f59cafdf5239bc5fd12acbfa
Pete Bingel <bingelp@gmail.com>
2019-03-22 16:49:17 -0400

    Prettier formatting

--- src/Components/Liquors/Liquor.js
+++ src/Components/Liquors/Liquor.js
@@ -1,26 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Liquor extends Component {
   render(props) {
-      const utm = "?utm_source=charleston_pride&utm_medium=toast_to_equality&utm_campaign=sponsors"
+    const utm =
+      "?utm_source=charleston_pride&utm_medium=toast_to_equality&utm_campaign=sponsors";
     return (
-        <div className="col-6 col-md-4 col-lg-2">
-            <div className="liquor">
-                <img src={this.props.image} alt={this.props.name + " logo"} className="img-fluid" />
-                <div className="gb-overlay">
-                    <div className="overlay-content">
-                        <div className="row">
-                            <div className="col">
-                            <h3 className="align-middle" >{this.props.name}</h3>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <a href={this.props.url + utm} target="_blank" rel="noopener noreferrer"><i className="fa fa-globe"></i></a>
-                        </div>
-                    </div>
+      <div className="col-6 col-md-4 col-lg-2">
+        <div className="liquor">
+          <img
+            src={this.props.image}
+            alt={this.props.name + " logo"}
+            className="img-fluid"
+          />
+          <div className="gb-overlay">
+            <div className="overlay-content">
+              <div className="row">
+                <div className="col">
+                  <h3 className="align-middle">{this.props.name}</h3>
                 </div>
+              </div>
             </div>
+          </div>
         </div>
-    )
+      </div>
+    );
   }
 }
--- src/Components/Sponsorships/Opportunity.js
+++ src/Components/Sponsorships/Opportunity.js
@@ -1,64 +1,90 @@
-import React, { Component } from 'react';
-import {Link} from 'react-router-dom'
-import Markdown from 'react-markdown'
-import NumberConverter from 'number-to-words'
-import './Opportunity.css'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Markdown from "react-markdown";
+import NumberConverter from "number-to-words";
+import "./Opportunity.css";
 
 class Opportunity extends Component {
   Price(props) {
     if (props.remaining === 0) {
-      return <h3 className="colored">SOLD OUT</h3>
+      return <h3 className="colored">SOLD OUT</h3>;
     }
-    return <h3 className="colored">${props.price.toLocaleString(navigator.language, { minimumFractionDigits: 0 })}</h3>
-  }
-  Remaining(props) {
-    if (props.count === 0) {
-      return <div>Sold Out</div>
-    }
-    if (props.count < 0 || props.count > 1) {
-      return <div>Avaliable</div>
-    }
-    return <div>{props.count} Remaining</div>
+    return (
+      <h3 className="colored">
+        $
+        {props.price.toLocaleString(navigator.language, {
+          minimumFractionDigits: 0
+        })}
+      </h3>
+    );
   }
 
   Benefits(props) {
-    return <ul>{React.Children.toArray(props.children).filter(o => o)}</ul>
+    return <ul>{React.Children.toArray(props.children).filter(o => o)}</ul>;
   }
 
   Tickets(props) {
-
     var count = props.count;
-    if(props.count < 10){
+    if (props.count < 10) {
       var word = NumberConverter.toWords(count);
       count = word.charAt(0).toUpperCase() + word.slice(1) + " (" + count + ")";
     }
 
     if (props.vip) {
-      return <li><strong>VIP Tickets:</strong> {count} (Includes open bar and catering)</li>
-    }
-    else {
-      return <li><strong>General Admission Tickets:</strong> {count}</li>
+      return (
+        <li>
+          <strong>VIP Tickets:</strong> {count} (Includes open bar and catering)
+        </li>
+      );
+    } else {
+      return (
+        <li>
+          <strong>General Admission Tickets:</strong> {count}
+        </li>
+      );
     }
   }
 
   Stage(props) {
-    return props.featured ? <li><strong>Stage:</strong> Logo Featured</li> : null;
+    return props.featured ? (
+      <li>
+        <strong>Stage:</strong> Logo Featured
+      </li>
+    ) : null;
   }
 
   Website(props) {
     if (props.website && props.website === "both") {
-      return <li><strong>Website:</strong> Logo placement on ToastToEquality.com &amp; <a href="https://charlestonpride.org">CharlestonPride.org</a></li>;
+      return (
+        <li>
+          <strong>Website:</strong> Logo placement on ToastToEquality.com &amp;{" "}
+          <a href="https://charlestonpride.org">CharlestonPride.org</a>
+        </li>
+      );
     }
-    return <li><strong>Website:</strong> Logo placement on ToastToEquality.com</li>;
+    return (
+      <li>
+        <strong>Website:</strong> Logo placement on ToastToEquality.com
+      </li>
+    );
   }
 
   Social(props) {
-    if(props.count > 10)
-    return <li><strong>Social Media:</strong> {props.count} posts/mentions minimum</li>
+    if (props.count > 10)
+      return (
+        <li>
+          <strong>Social Media:</strong> {props.count} posts/mentions minimum
+        </li>
+      );
 
     var word = NumberConverter.toWords(props.count);
     word = word.charAt(0).toUpperCase() + word.slice(1);
-    return <li><strong>Social Media:</strong> {word} ({props.count}) {props.count > 1 ? "posts/mentions" : "post/mention"} minimum</li>
+    return (
+      <li>
+        <strong>Social Media:</strong> {word} ({props.count}){" "}
+        {props.count > 1 ? "posts/mentions" : "post/mention"} minimum
+      </li>
+    );
   }
 
   Marketing(props) {
@@ -97,49 +123,83 @@ class Opportunity extends Component {
 
     message += ".";
 
-    return <li><strong>Marketing:</strong> {message}</li>
-
+    return (
+      <li>
+        <strong>Marketing:</strong> {message}
+      </li>
+    );
   }
 
   Advertising(props) {
-
     const pridePrices = { full: 750, half: 500, quarter: 350 };
-    const toastPrices = { full: 400, half: 300, "one-third": 200, quarter: 150 };
+    const toastPrices = {
+      full: 400,
+      half: 300,
+      "one-third": 200,
+      quarter: 150
+    };
     var messages = [];
 
     if (props.guide) {
-      messages.push(`One (1) ${props.guide} page ad in Charleston Pride Guide ($${pridePrices[props.guide]} value).`)
+      messages.push(
+        `One (1) ${props.guide} page ad in Charleston Pride Guide ($${
+          pridePrices[props.guide]
+        } value).`
+      );
     }
     if (props.program) {
-      messages.push(`One (1) ${props.program} page ad in Toast To Equality Program ($${toastPrices[props.program]} value).`)
+      messages.push(
+        `One (1) ${props.program} page ad in Toast To Equality Program ($${
+          toastPrices[props.program]
+        } value).`
+      );
     }
     if (messages.length === 0) {
       return null;
     }
-    return <li><strong>Advertising:</strong> {messages.join(` `)} <Link to="/advertise">More Details</Link></li>;
+    return (
+      <li>
+        <strong>Advertising:</strong> {messages.join(` `)}{" "}
+        <Link to="/advertise">More Details</Link>
+      </li>
+    );
   }
 
   render(props) {
     return (
       <div className="opportunity col-lg-6">
         <h2>{this.props.data.title} Sponsor</h2>
-        <this.Price price={this.props.data.price} remaining={this.props.data.remaining} />
-        <div className="description"><Markdown source={this.props.data.description} /></div>
+        <this.Price
+          price={this.props.data.price}
+          remaining={this.props.data.remaining}
+        />
+        <div className="description">
+          <Markdown source={this.props.data.description} />
+        </div>
         <this.Benefits>
-          <this.Tickets vip={this.props.data.vip} count={this.props.data.tickets} />
+          <this.Tickets
+            vip={this.props.data.vip}
+            count={this.props.data.tickets}
+          />
           <this.Stage featured={this.props.data.featured} />
           <this.Website website={this.props.data.web} />
           <this.Social count={this.props.data.posts} />
-          <this.Marketing email={this.props.data.email} radio={this.props.data.radio} print={this.props.data.print} television={this.props.data.television} />
-          <this.Advertising guide={this.props.data.guide} program={this.props.data.program} />
+          <this.Marketing
+            email={this.props.data.email}
+            radio={this.props.data.radio}
+            print={this.props.data.print}
+            television={this.props.data.television}
+          />
+          <this.Advertising
+            guide={this.props.data.guide}
+            program={this.props.data.program}
+          />
         </this.Benefits>
       </div>
     );
   }
 }
 
-Opportunity.propTypes = {
-
-};
+Opportunity.propTypes = {};
 
 export default Opportunity;
